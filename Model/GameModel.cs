7ea0d335a46271be11d7ed3dbc79c50e6b2aecb5
Fejanos <fenyvesijani97@gmail.com>
2025-09-14 15:56:57 +0200

    Model class

---
+++ Model/GameModel.cs
@@ -0,0 +1,7 @@
+public class GameResult
+{
+    public string PlayerName { get; set; } = string.Empty;
+    public int SecretNumber { get; set; }
+    public int Attempts { get; set; }
+    public DateTime PlayedAt { get; set; } = DateTime.Now;
+}
