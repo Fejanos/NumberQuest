84a2571dabd034214a28575a6b8a87f57b1cfdd0
Fejanos <fenyvesijani97@gmail.com>
2025-09-14 16:19:30 +0200

    Program setup and MVC pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NumberQuest.Model;
+using NumberQuest.View;
+using NumberQuest.Data;
+using NumberQuest.Controller;
+
+GameView view = new GameView();
+FileStorage storage = new FileStorage();
+GameController controller = new GameController(view, storage);
+
+controller.Run();
